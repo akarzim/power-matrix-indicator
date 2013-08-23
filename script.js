defb2aee7394838ef9ebff370e35d096441f4b29
peernohell <peernohell@gmail.com>
2013-08-23 08:17:08 +0200

    remove jquery dependancy

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
-$(document).ready(function ($) {
-    $.fn.mood = function () {
-        var i, moods = ['happy', 'neutral', 'sad'];
-        for (i = 0; i < moods.length; i++) {
-            if (this.hasClass(moods[i])) { return moods[i]; }
-        }
-        return null;
-    }
+// Power Matrix Tool namespace
+PMT = {};
 
-     function updateIndicator (el) {
-        var mood = $(el).mood();
-        if (mood) { icon.setAttribute('href', chrome.extension.getURL(mood + '.png')); }
-    }
+PMT.moodList = ['happy', 'neutral', 'sad'];
+PMT.mood = function (moodEl) {
+	return PMT.moodList.reduce(function (prev, mood) { return prev || moodEl.classList.contains(mood) && mood}, false);
+};
 
-		// create an observer
-		var
-		happiness = document.getElementById('happiness'),
-		icon = document.querySelector('link[rel="icon"]'),
-		updateIndicatorOnClassMutation = function (mutation) {
-			if (mutation.type === 'attributes' && mutation.attributeName === 'class' ) {
-				updateIndicator(mutation.target);
-			}
-		},
-		observer = new MutationObserver(function(mutations) {
-			mutations.forEach(this);
-		}.bind(updateIndicatorOnClassMutation));
-		// observe happiness
-		observer.observe(happiness, {attributes: true});
-		updateIndicator(happiness);
+PMT.updateIndicator = function (target, moodEl) {
+		var mood = PMT.mood(moodEl);
+		if (mood) { target.setAttribute('href', chrome.extension.getURL(mood + '.png')); }
+}
 
+// create an observer
+var
+// find dom element needed
+happiness = document.getElementById('happiness'),
+icon = document.querySelector('link[rel="icon"]'),
+// bind updateIndicator to icon
+updateIconIndicator = PMT.updateIndicator.bind(null, icon),
+updateIndicatorOnClassMutation = function (mutation) {
+	if (mutation.type === 'attributes' && mutation.attributeName === 'class') {
+		updateIconIndicator(mutation.target);
+	}
+},
+observer = new MutationObserver(function(mutations) {
+	mutations.forEach(updateIndicatorOnClassMutation);
 });
+// observe happiness
+observer.observe(happiness, {attributes: true});
+updateIconIndicator(happiness);
